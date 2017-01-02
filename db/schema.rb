c40e8dce4317fac6432d20b2ee1d0d198399fbc5
L.Joel <leopard10000@gmail.com>
2017-01-02 13:56:32 +0000

    docker-compose: web and db(postgres) container.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170102111131) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
     t.integer  "price"
